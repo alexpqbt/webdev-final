49ef1200e7ecc4fb198d1c42b27456ad60bf5a74
Andrei Fox <115098095+alexpqbt@users.noreply.github.com>
2023-12-15 23:59:30 +0800

    change homepage

---
+++ js/index.js
@@ -0,0 +1,10 @@
+let prevScrollpos = window.scrollY;
+window.onscroll = function() {
+  let currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-90px";
+  }
+  prevScrollpos = currentScrollPos;
+}
